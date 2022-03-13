b951226a05f3cd3bf2f1d684772d0deae62b7540
Frederick Dong <fredisawesome147@gmail.com>
2022-03-13 00:15:48 -0800

    fixes

--- lambda.js
+++ lambda.js
@@ -1,10 +1,4 @@
-// import setup from './server'
 const serverlessExpress = require('@vendia/serverless-express')
 const setup = require('./server')
 
-const cors_enabled = setup(true)
-const cors_disabled = setup(false)
-
-exports.handler = serverlessExpress({ app: cors_enabled })
-
-exports.handler_beta = serverlessExpress({ app: cors_disabled })
+exports.handler = serverlessExpress({ app: setup(process.env.CORS_ENABLED === 'true') })
