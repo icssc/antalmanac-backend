a1c7c52df023bdd2f94b2579b79ba60eec88df6f
Frederick Dong <fredisawesome147@gmail.com>
2022-04-11 16:33:53 -0700

    DDB session store

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require('./routes');
 const cors = require('cors');
 const passport = require('passport');
 const session = require('express-session');
+const DynamoDBStore = require('dynamodb-store');
 
 require('dotenv').config();
 
@@ -16,7 +17,13 @@ const setup = (corsEnabled) => {
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
-        cookie: { maxAge: 1000 * 60 * 60 * 24 },
+        // cookie: { maxAge: 1000 * 60 * 60 * 24 },
+        store: new DynamoDBStore({
+            'table': {
+                'name': process.env.SESSION_DDB_NAME,
+                'ttl': 1000 * 60 * 60 * 24
+            }
+        })
       }));
     app.use(passport.initialize());
     app.use(passport.session());
