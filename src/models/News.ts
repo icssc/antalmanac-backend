4a41eca21c58ecc4ae87195600c8e63ad370dbb8
senti <brianmv@uci.edu>
2023-02-17 07:05:52 -0800

    removed models

--- src/models/News.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const NewsSchema = new mongoose.Schema({
-  title: String,
-  body: String,
-  date: String,
-})
-
-export default mongoose.model('News', NewsSchema)
--- src/models/Notification.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const NotificationSchema = new mongoose.Schema({
-  sectionCode: String,
-  courseTitle: String,
-  phoneNumbers: [String],
-})
-
-export default mongoose.model('Notification', NotificationSchema)
